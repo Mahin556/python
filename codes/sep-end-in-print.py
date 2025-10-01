d532beb5580cd3103d4fd045ab730aca7b636c9e
Mahin556 <80610763+Mahin556@users.noreply.github.com>
2025-10-01 15:01:31 +0530

    Update sep-end-in-print.py

--- codes/sep-end-in-print.py
+++ codes/sep-end-in-print.py
@@ -34,3 +34,22 @@ Explanation: a is printed n=5 times in a single line without space between them.
 print(a*n,sep="",end="\n")
 
 ###############################################################################################################################################################
+
+#User function Template for python3
+a = input()
+b = input()
+c = input()
+# Write your code below that prints a a times and b b times, seperated by c
+"""
+Given three inputs that are stored in the variables a, b, and c. You need to print a a times and b b times  in a single line separated by c.
+
+Examples:
+
+Input: a = 6, b = 4, c = &
+Output: 666666&4444
+Explanation: 6 printed 6 times and 4 printed 4 times seperated by c = &.
+"""
+
+print(a*int(a),b*int(b),sep=c)
+
+###############################################################################################################################################################
